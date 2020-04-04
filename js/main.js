a0b023adbb23af4e2f14821c8eb6382fe3366ab7
Al-Fredicus <63160620+Al-Fredicus@users.noreply.github.com>
2020-04-04 21:20:23 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
-let ventana = document.getElementById('ventana-flotante');
+let modal = document.getElementById('directos');
 let flex = document.getElementById('flex');
 let abrir = document.getElementById('abrir');
 let cerrar = document.getElementById('close');
 
 abrir.addEventListener('click', function(){
-    ventana.style.display = 'block';
+    modal.style.display = 'block';
 });
 
 cerrar.addEventListener('click', function(){
-    ventana.style.display = 'none';
+    modal.style.display = 'none';
 });
 
 window.addEventListener('click', function(e){
     console.log(e.target);
     if(e.target == flex){
-        ventana.style.display = 'none';
+        modal.style.display = 'none';
     }
 });
+
