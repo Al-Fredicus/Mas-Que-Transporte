23a55ee071c235fa94f2548aa7c70197bdff8b0a
msola-okn <msola@openknowledgenetwork.com>
2020-04-06 19:50:12 +0200

    responsive

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
-let modal = document.getElementById('directos');
-let flex = document.getElementById('flex');
-let abrir = document.getElementById('abrir');
-let cerrar = document.getElementById('close');
+// let modal = document.getElementById('directos');
+// let flex = document.getElementById('flex');
+// let abrir = document.getElementById('abrir');
+// let cerrar = document.getElementById('close');
 
-abrir.addEventListener('click', function(){
-    modal.style.display = 'block';
-});
+// abrir.addEventListener('click', function () {
+//     modal.style.display = 'block';
+// });
 
-cerrar.addEventListener('click', function(){
-    modal.style.display = 'none';
-});
+// cerrar.addEventListener('click', function () {
+//     modal.style.display = 'none';
+// });
 
-window.addEventListener('click', function(e){
-    console.log(e.target);
-    if(e.target == flex){
-        modal.style.display = 'none';
-    }
-});
+// window.addEventListener('click', function (e) {
+//     console.log(e.target);
+//     if (e.target == flex) {
+//         modal.style.display = 'none';
+//     }
+// });
 
+
+$(function () { // DOM ready
+    // Toggle open and close nav styles on click
+    $('#menu-toggle').click(function () {
+        $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#menu-toggle').on('click', function () {
+        this.classList.toggle('active');
+    });
+}); // end DOM ready
